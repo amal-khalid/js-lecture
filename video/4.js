6bb6e39ac7e745bfaaa90f571e3da44fd58eb5ed
amal khalid <khalidamal237@gmail.com>
2025-03-19 08:58:39 -0700

    project

---
+++ video/4.js
@@ -0,0 +1,11 @@
+const btn=document.querySelector(".btn");
+const trailercontainer=document.querySelector(".trailer-container");
+
+btn.addEventListener("click",() => {
+    trailercontainer.classList.remove
+    ("active");
+}
+
+
+
+);
